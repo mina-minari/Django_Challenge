f9924a1adfc30a73eb4ae233be8de3761a87ccd3
parksiwoo2 <132458293+parksiwoo2@users.noreply.github.com>
2025-11-16 14:06:10 +0000

    login#1

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    #profile_image = models.ImageField()
+    challenge_point=models.IntegerField(default=0)
+    #challenge=models.ManyToManyField()
+
+    def __str__(self):
+        return self.username
--- project/app/views.py
+++ project/app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+def index(request):
+    return render(request, 'app/login.html')
+def google_login(request):
+    params={
+        'client_id': 
+    }
+    auth_url="https://accounts.google.com/o/oauth2/v2/auth?client_id:"192516169686-pirpgfr064aukj0nranq46o6spn6rvli.apps.googleusercontent.com"&redirect_uri:"http://localhost:8000/google/callback"&response_type=code&scope=email%20profile%20openid"
+    return render(request, 'https://accounts.google.com/o/oauth2/auth')
