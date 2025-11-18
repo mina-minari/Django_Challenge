e4065825098b1411c4ffae9ab9ce1917117cb88b
hi-coco11 <giho04300@gmail.com>
2025-11-18 22:38:50 +0900

    edit  models.py regarding challenge, verification and views.py

--- project/app/views.py
+++ project/app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.urls import reverse
+from django.db.models import Count
+
+from .models import Challenge, Verification
+## form 부분도 import 해야함
 
 # Create your views here.
+
+def challenge_list(request):
+    """
+    생성된 챌린지 전체 목록 보기 (READ)
+    """
+    challenges = Challenge.objects.all().order_by('-created_at')
+    return render(request, 'challenge/challenge_list.html', {
+        'challenges': challenges,
+    })
